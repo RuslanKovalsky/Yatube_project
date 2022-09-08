673964a7ddf817e8d344d7cb66791847e07e38c2
Ruslan Kovalsky <ruslan.kovalsky@gmail.com>
2022-09-08 19:00:34 +0300

    v 1.0.6 new pages

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'posts'
+
 urlpatterns = [
     path('', views.index),
-    path('posts/', views.group_list),
+    #path('posts/', views.group_list, name='group_list'),
+    path('posts/', views.group_list, name='group_list'),
 ]
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -22,6 +22,7 @@ from django.urls import include, path
 
 urlpatterns = [
     # импорт правил из приложения posts
-    path('', include('posts.urls')),
+    #path('', include('posts.urls')),
+    path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
 ]
