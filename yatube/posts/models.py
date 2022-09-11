b1e0daf8c428fb5f20c32b6a5b78c9521a473695
RuslanKovalsky <Ruslankovalsky@gmail.com>
2022-09-11 20:35:56 +0300

    version 1.1.0 add css and bd

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+# Из модуля auth импортируем функцию get_user_model
+from django.contrib.auth import get_user_model
+#Для работы с моделями импортируется модуль models,
+# а для создания поля со ссылкой на модель User
+# импортируется и эта модель: она встроена в
+# Django и отвечает за управление пользователями.
+#Официальная документация рекомендует
+#обращаться к модели User через функцию g
+#et_user_model. Следуем этой рекомендации:
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
